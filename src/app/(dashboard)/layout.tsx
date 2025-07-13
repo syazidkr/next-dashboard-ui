5bf905c698d21ab1f4e41f1ff158e457b5508766
syazidkr <111582922+syazidkr@users.noreply.github.com>
2025-07-13 03:31:22 +0000

    Modify Navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,10 +16,13 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32}></Image> 
           <span className="hidden lg:block">SchooLama</span>      
         </Link>
-        <Menu/>
+        <Menu />
       </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">right</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
    </div>
   );
 }
