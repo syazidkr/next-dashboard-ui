a8fcf187c9d0662456ce8661f4682a9a1bd76644
syazidkr <111582922+syazidkr@users.noreply.github.com>
2025-07-13 04:34:14 +0000

    Create UserAdd components

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,9 @@
 const AdminPage = () => {
   return (
-    <div className=''>AdminPage</div>
+    <div className='p-4 gap-4 flex flex-col md:flex-row'>
+      <div className="bg-red-300 h-full w-full lg:w-2/3">Main</div>
+      <div className="bg-blue-400 h-full w-full lg:w-1/3">Right</div>
+    </div>
   )
 }
 
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,7 @@
+const UserCard = ({type}:{type:string}) => {
+  return (
+    <div>UserCard</div>
+  )
+}
+
+export default UserCard
