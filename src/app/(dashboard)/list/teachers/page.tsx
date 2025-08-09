2b30f5116ac5c0db6cf2985a127949f91e5a7fbb
syazidkr <111582922+syazidkr@users.noreply.github.com>
2025-08-09 08:08:15 +0000

    Create Pangination for list

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,4 +1,5 @@
 import TableSearch from "@/components/TableSearch"
+import Pagination from "@/components/Pagination"
 import Image from "next/image"
 
 const TeacherListPage = () => {
@@ -9,19 +10,23 @@ const TeacherListPage = () => {
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch/>
-          <div className="">
-            <button>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src='/filter.png' alt='' width={14} height={14}/>
             </button>
-            <button></button>
-            <button></button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src='/sort.png' alt='' width={14} height={14}/>
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src='/plus.png' alt='' width={14} height={14}/>
+            </button>
           </div>
         </div>
       </div>
       {/* LIST */}
       <div className=""></div>
-      {/* PAFINATION */}
-      <div className=""></div>
+      {/* PAGINATION */}
+      <Pagination/>
     </div>
   )
 }
